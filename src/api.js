c446cf6e052191bf31546279b717b022f5a059ec
moosison <49648924+moosison@users.noreply.github.com>
2021-02-18 23:27:01 +0200

    apitest

--- src/api.js
+++ src/api.js
@@ -1,14 +1,43 @@
 const express = require("express");
 const serverless = require("serverless-http");
+const axios = require('axios');
 
 const app = express();
 const router = express.Router();
 
+const key = 'key';
+const vendor = 'onet;'
+
+// function getparams(params) {
+//     const { CallID, CallerNum, CallerIDNum, CalledID, CalledExtention, CallStatus, CallFlow, CallerExtention, CalledNumber, CallAPIID } = req.params;
+
+
+// }
+// /call/log?action=ring&from_phone=<FROM_PHONE>&target_phone=<TARGET_PHONE>&call_id=<CALL_ID>
+//     &extension=<EXTENSION>
+//     &key=<KEY>&vendor=<VENDOR>&is_outgoing=0
+
 router.get("/", (req, res) => {
-    res.json({
-        hello: "hi!"
-    });
+    const url = req.params;
+
+    const getData = async(url) => {
+        try {
+            const response = await axios.get(url)
+            const data = response.data;
+            res.send(data)
+        } catch (error) {
+            res.send(error)
+        }
+    }
+
+    // getData(url)
+
+    // res.json({
+    //     hello: "hi!"
+    // });
 });
+
+
 
 app.use(`/.netlify/functions/api`, router);
 
