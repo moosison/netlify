81a132e5565cc0de52f806006d14229b6745d055
moosison <49648924+moosison@users.noreply.github.com>
2021-02-20 20:13:44 +0200

    Update api.js

--- src/api.js
+++ src/api.js
@@ -29,7 +29,7 @@ router.get("/", (req, res) => {
         let target_phone = CalledNumber;
     };
     let call_id = CallAPIID;
-    let from_phone = CallerNum;
+
     let extension = CalledExtention;
     res.send(action, target_phone, call_id, from_phone, extension);
 
